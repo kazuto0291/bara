060c41c8ca8254a7d47be407dc01c51f0f66d11e
kazuto0291 <zutozuto03170291@gmail.com>
2020-04-10 19:48:18 +0900

    ToDoListltemコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ToDoListItem from "./ToDoListltem.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          書き換えました
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <ToDoListItem
+            title="ホームページ作成"
+            description="サークルホームページを今週中に作成する"
+            />
+        </div>
+      </div>
+
+    );
+  }
 }
 
 export default App;
